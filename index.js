be190df58e52b9702757fd96e75f334108343c90
Demarcus <demarcusprophet@gmail.com>
2023-01-04 01:58:08 -0600

    commit tree 4b83da9536cd41d3aef797eec49b007a8ba37e0e

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const mainElement = document.querySelector('main#main');
+if (mainElement) {
+  mainElement.parentNode.removeChild(mainElement);
+}
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+const name = 'Demarcus';
+newHeader.innerHTML = `${name} is the champion`;
+
+document.body.appendChild(newHeader);
