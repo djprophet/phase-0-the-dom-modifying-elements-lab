7aae381d2cd4e678fd96b51eec161b75e39878d7
Demarcus Prophet <demarcusprophet@gmail.com>
2023-01-31 12:08:32 -0600

    Dom elements

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "YOUR-NAME is the champion" 
+
+main.remove()
+
